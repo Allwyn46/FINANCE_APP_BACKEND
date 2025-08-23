e4851a51c6990e05d877335ea0833db4b8b59f8b
allwyn <allwyn153@gmail.com>
2025-08-23 09:59:02 +0530

    Reworked server.js and initialized all dependencies

---
+++ src/server.js
@@ -0,0 +1,40 @@
+import express, { json, urlencoded } from "express";
+import session from "express-session";
+import passport from "passport";
+import dotenv from "dotenv";
+import cors from "cors";
+
+dotenv.config();
+
+const app = express();
+
+// MIDDLEWARES
+const corsOptions = {
+  origin: ["http://localhost:5173"],
+  Credential: true,
+};
+app.use(cors(corsOptions));
+app.use(json({ limit: "100mb" }));
+app.use(urlencoded({ limit: "100mb", extended: true }));
+// SESSION
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET || "secret_124",
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      maxAge: 60000 * 60,
+    },
+  }),
+);
+// PASPORT
+app.use(passport.initialize());
+app.use(passport.session());
+
+// ROUTES
+
+// APP LISTEN
+const port = process.env.APP_PORT || 7001;
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
